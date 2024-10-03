135d6b8a7d2ff6c83118271c10e1f39f38c07dcd
Damian Kaczorowski <kaczorowskid97@gmail.com>
2024-10-03 20:59:10 +0200

    refactor: remove DevicsTableV2

--- src/components/app/DevicesTableV2/DevicesTable.types.ts
+++
@@ -1,3 +0,0 @@
-export type DevicesTableProps = {
-  isDashboardPart?: boolean;
-};
--- src/components/app/DevicesTableV2/ExtraButton/ExtraButton.types.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-
-export type ExtraButtonProps = {
-  isDashboardPart: boolean;
-  searchbarValue: string;
-  setSearchbarValue: Dispatch<SetStateAction<string>>;
-};
