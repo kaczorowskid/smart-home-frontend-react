c4a745c0914060e941169e553442ab52025f5db7
Damian Kaczorowski <kaczorowskid97@gmail.com>
2024-10-03 20:56:02 +0200

    refactor: remove old table of devices

--- src/components/DevicesTable/NewDeviceItem/NewDeviceItem.types.ts
+++
@@ -1,7 +0,0 @@
-import { SetStateAction } from "react";
-
-export type NewDeviceItemProps = {
-  selectedValue: string;
-  setSelectedValue: (value: SetStateAction<string>) => void;
-  isDashboardPart: boolean;
-};
--- src/components/DevicesTable/TableHeader/TableHeader.types.ts
+++
@@ -1,8 +0,0 @@
-import { DeviceType } from "@/types/common.types";
-
-export type TableHeaderProps = {
-  type?: DeviceType;
-  name?: string;
-  status?: string;
-  id?: string;
-};
