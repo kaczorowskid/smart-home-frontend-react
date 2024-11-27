b1a2ed423bf4d6344c3efa42b0b702133980bd65
Damian Kaczorowski <kaczorowskid97@gmail.com>
2024-11-27 13:31:30 +0100

    fix: fix appliaction issues

--- src/contexts/Localization/Localization.context.ts
+++ src/contexts/Localization/Localization.context.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
-import { Locale, LocalizationContextType } from "./Localization.types";
+import { LocalizationContextType } from "./Localization.types";
 
 export const LocalizationContext = createContext<LocalizationContextType>({
   locale: "en",
-  setUserLocale: (locale: Locale) => {},
+  setUserLocale: () => {},
 });
--- src/contexts/Theme/Theme.context.ts
+++ src/contexts/Theme/Theme.context.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
-import { ThemeContextType, ThemeOption } from "./Theme.types";
+import { ThemeContextType } from "./Theme.types";
 
 export const ThemeContext = createContext<ThemeContextType>({
   theme: "dark",
-  setTheme: (theme: ThemeOption) => {},
+  setTheme: () => {},
 });
