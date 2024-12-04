1ad88144075cdc27780111d545a23255c8e18048
Damian Kaczorowski <kaczorowskid97@gmail.com>
2024-12-04 16:48:40 +0100

    infra: move server_name section to the configuration on the VPS

--- src/views/start/Header/Header.tsx
+++ src/views/start/Header/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => (
   <header className="px-10 h-[90px] text-5xl flex items-center justify-center lg:justify-between">
     <div>
       <span className="text-custom-salmon font-bold">S</span>mart
-      <span className="text-custom-cranberry font-bold">H</span>ouse
+      <span className="text-custom-cranberry font-bold">H</span>ome
     </div>
   </header>
 );
