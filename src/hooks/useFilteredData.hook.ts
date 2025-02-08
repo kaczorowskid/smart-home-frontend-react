eb735f551f133068f2a9bdc8233fb734c65b88dd
Damian Kaczorowski <kaczorowskid97@gmail.com>
2025-02-08 15:24:41 +0100

    hotfix: use useMemo in useFilteredData hook

--- src/hooks/useFilteredData.hook.ts
+++ src/hooks/useFilteredData.hook.ts
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export const useFilteredData = <T extends Record<string, unknown>>(
   data?: T[]
 ) => {
   const [searchbarValue, setSearchbarValue] = useState("");
 
-  const filteredData = data?.filter((item) =>
-    Object.values(item).some((value) =>
-      value
-        ?.toString()
-        .trim()
-        .toLowerCase()
-        .includes(searchbarValue.trim().toLowerCase())
-    )
+  const filteredData = useMemo(
+    () =>
+      data?.filter((item) =>
+        Object.values(item).some((value) =>
+          value
+            ?.toString()
+            .trim()
+            .toLowerCase()
+            .includes(searchbarValue.trim().toLowerCase())
+        )
+      ),
+    [data, searchbarValue]
   );
 
   return {
